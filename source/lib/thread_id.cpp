ba9ba1b6d73990e40e9a486e7b90e223b2632b9f
Joe Wu <wuzongsheng@gmail.com>
2021-01-10 14:46:53 +0800

    add thread_id

---
+++ source/lib/thread_id.cpp
@@ -0,0 +1,42 @@
+
+#include "thread_id.hpp"
+
+#include <atomic>
+#include <com/fatal_error.hpp>
+#include <com/predict.hpp>
+
+namespace miu::log {
+
+static std::atomic<uint8_t> g_mask { 0 };
+thread_local int32_t g_id { -1 };
+
+constexpr int32_t
+thread_id::max() {
+    return 7;
+}
+
+int32_t
+thread_id::get() {
+    if (UNLIKELY(g_id < 0)) {
+        for (g_id = 0; g_id < thread_id::max(); g_id++) {
+            auto mask = 1U << g_id;
+            if (!(g_mask.fetch_or(mask) & mask)) {
+                return g_id;
+            }
+        }
+
+        FATAL_ERROR("too many threads");
+    }
+
+    return g_id;
+}
+
+void
+thread_id::reset() {
+    if (g_id >= 0) {
+        g_mask.fetch_and(~(1 << g_id));
+        g_id = -1;
+    }
+}
+
+}    // namespace miu::log
---
+++ unittest/ut_thread_id.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+#include <condition_variable>
+#include <future>
+#include <mutex>
+#include <thread>
+
+#include "source/lib/thread_id.hpp"
+
+using miu::log::thread_id;
+
+TEST(ut_thread_id, max) { EXPECT_EQ(7, thread_id::max()); }
+
+TEST(ut_thread_id, get) {
+    EXPECT_EQ(0U, thread_id::get());
+    EXPECT_EQ(0U, thread_id::get());
+
+    std::thread { []() {
+        EXPECT_EQ(1U, thread_id::get());
+        EXPECT_EQ(1U, thread_id::get());
+        thread_id::reset();
+    } }.join();
+
+    thread_id::reset();
+}
+
+TEST(ut_thread_id, overflow) {
+    std::mutex mtx;
+    std::condition_variable cv;
+
+    for (auto i = 0; i < thread_id::max(); i++) {
+        std::promise<bool> ready;
+        std::thread([&]() {
+            EXPECT_EQ(i, thread_id::get());
+            ready.set_value(true);
+            std::unique_lock<std::mutex> l { mtx };
+            cv.wait(l);
+            thread_id::reset();
+        }).detach();
+        ready.get_future().get();
+    }
+
+    EXPECT_ANY_THROW(thread_id::get());
+
+    cv.notify_all();
+}
